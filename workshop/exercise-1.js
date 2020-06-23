b22cf25da890d439043c3b27ce1c464942e2084b
Antonio Placido <aplacido1992@gmail.com>
2020-06-23 13:56:14 -0400

    Question 1

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -12,3 +12,27 @@ let mostPopularFood = [
   'parmesan chicken',
   undefined,
 ];
+
+const fixList = (arr, first, last) => {
+  const theFoods = [...arr];
+  mostPopularFood.pop();
+  mostPopularFood.unshift(`bean burritos`);
+  mostPopularFood.push(`buffalo-flavored cauliflower`);
+  console.log(mostPopularFood);
+  return theFoods;
+  console.log(theFoods);
+};
+
+
+const printList = (arr) => {
+  for (let i=1;i<= arr.length; i++)
+  {
+    console.log(`${i} - ${arr[i]}`);
+  }
+}
+
+
+//step 3
+
+printList(fixList(mostPopularFood, `bean burritos`, `buffalo-flavored cauliflower`));
+
