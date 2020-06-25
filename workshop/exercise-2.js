d2347fbb6ab652b7d908d8fba3ba62319634b970
Antonio Placido <aplacido1992@gmail.com>
2020-06-25 10:03:45 -0400

    Question 2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,3 +3,15 @@
 
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+const totallist = earlyBirds.concat(lateComers);
+
+// console.log(totallist);
+
+
+const printGuestOrder = (earlyBirds, lateComers) => {
+    for (let i = 0; i<totallist.length; i++){
+        console.log(`${i+1} - ${totallist[i]}`)
+    }
+}
+
+printGuestOrder(totallist);
