133610f741c895808cbad5ee864a17ae4ba688c1
Antonio Placido <aplacido1992@gmail.com>
2020-06-28 16:42:48 -0400

    Question 7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -2,15 +2,40 @@
 // ----------
 
 let foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
 };
 
 // createRecommendations function
 
-// printRecommendations function
+//convert the object into an array;//
 
-// function call (done)
+let newFoodArray = Object.entries(foodPairings); //makes an array of arrays
+// console.log(newFoodArray);
+
+const createRecommendations = (thesort) => {
+  return Object.keys(thesort).map((food) => {
+    return `With ${food}, you should have ${thesort[food]}.`;
+  });
+};
+
+const printRecommendations = (entries) => {
+  entries.forEach((entry) => {
+    console.log(`${entry}`);
+  });
+};
+
+let rows = newFoodArray.length; // gives 4//
+// let newMods = newFoodArray.toString();
+
+// console.log(newMods);
+
+// function createRecommendations
+
+// console.log(createRecommendations(foodPairings, `burgers`));
+// // printRecommendations function
+
+// // function call (done)
 printRecommendations(createRecommendations(foodPairings));
